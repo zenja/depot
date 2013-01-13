b06a21660a958506b77d5e8c88a8c8f8faf7b0bc
Xing, Wang <wang.xing.me@gmail.com>
2013-01-13 17:48:23 +0100

    push code so far

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '7c07a61738d4b1a33bb8665a3511b78cf8978493355310b4e9fe3512f1fa39f58652fc0ef963a4fd2cd89a58e1ee197d0969ea929277f977fc40dfc8a606f1f7'
