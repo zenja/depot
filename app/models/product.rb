9fb9d0e7abfec5dc42adc1fd60f22603d76f71c2
Xing, Wang <wang.xing.me@gmail.com>
2013-01-13 21:06:23 +0100

    finish Task B: validation and unit testing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, :price, :presence => true
+  validates :price, :numericality => {:great_than_or_equal_to => 0.01}
+  validates :title, :uniqueness => true
+  validates :image_url, :format => {
+    :with => %r{\.(gif|jpg|png)$}i,
+    :message => "must be a url for GIF, JPG, or PNG image"
+  }
 end
