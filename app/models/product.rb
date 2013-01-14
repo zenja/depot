0cbf3e78ca9ffe2053998b4b2d2e53c8e3e7a553
Xing, Wang <wang.xing.me@gmail.com>
2013-01-14 16:56:17 +0100

    finish Task D: Cart Creation

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,19 @@ class Product < ActiveRecord::Base
     :with => %r{\.(gif|jpg|png)$}i,
     :message => "must be a url for GIF, JPG, or PNG image"
   }
+
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_items
+
+  private
+
+  def ensure_not_referenced_by_any_line_items
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line item present')
+      return false
+    end
+  end
+
 end
