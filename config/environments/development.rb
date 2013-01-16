ae10d9d71e0346c9a81484ad740ffbe688cd9cd4
Xing, Wang <wang.xing.me@gmail.com>
2013-01-16 18:59:38 +0100

    finish Task H - Iteration H1: Sending Confirmation Emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,17 @@ Depot::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Configurations about sending mail, added by zenja
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "domain.of.sender.net",
+    authentication: "plain",
+    user_name: "wx.dev.test",
+    password: "zaqwsxcderfvb",
+    enable_starttls_auto: true
+  }
+
 end
