f2e0316c9fb5c7d7aa37be40177a3646cfb1af11
Xing, Wang <wang.xing.me@gmail.com>
2013-01-17 14:33:50 +0100

    finish Task I - Iteration I1: Adding Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130115180234) do
+ActiveRecord::Schema.define(:version => 20130116185332) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -54,5 +54,12 @@ ActiveRecord::Schema.define(:version => 20130115180234) do
 
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
 
 end
